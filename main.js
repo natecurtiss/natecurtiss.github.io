d2ffe1db6566346801d8a1d2b3c57b03bf1dbb16
natecurtiss <n8dev.cs@gmail.com>
2023-04-24 14:43:22 -0400

    It's good enough

--- main.js
+++ main.js
@@ -5,13 +5,16 @@ function submitted() {
         return;
     let form = document.forms["gform"]["entry.38295861"];
     const reg = /\S+@\S+\.\S+/;
-    if (!reg.test(form.value.trim()))
+    if (!reg.test(form.value.trim())) {
         alert("Please enter a valid email!");
-    else
+    }
+    else {
         done = true;
-    $(document.getElementById("i1")).fadeOut(200);
-    $(document.getElementById("i2")).fadeOut(200);
-    $(document.getElementById("email-entry")).fadeOut(200);
+        $(document.getElementById("i1")).fadeOut(500);
+        $(document.getElementById("i2")).fadeOut(500);
+        $(document.getElementById("email-entry")).fadeOut(500);
+        $(document.getElementById("i3")).delay(500).fadeIn(500);
+    }
 }
 
 
