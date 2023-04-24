f5900c0b47f431386d21e038c3beb65402b18c86
natecurtiss <n8dev.cs@gmail.com>
2023-04-24 14:07:28 -0400

    Add buggy fade out

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 let done = false;
 
 function submitted() {
+    if (done)
+        return;
     let form = document.forms["gform"]["entry.38295861"];
-    $('#everything *').fadeOut(500);
-    done = true;
+    const reg = /\S+@\S+\.\S+/;
+    if (!reg.test(form.value.trim()))
+        alert("Please enter a valid email!");
+    else
+        done = true;
+    $(document.getElementById("i1")).fadeOut(200);
+    $(document.getElementById("i2")).fadeOut(200);
+    $(document.getElementById("email-entry")).fadeOut(200);
 }
 
 
